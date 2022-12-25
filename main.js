875638677a96ec2dc6a4649485635cb47f73fe5a
dhruvbeastboy2020@gmail.com <dhruvbeastboy2020@gmail.com>
2022-12-26 00:55:53 +0530

    init

---
+++ main.js
@@ -0,0 +1,44 @@
+const p = document.getElementById("mt")
+const txt = document.getElementById("txt")
+const btn = document.getElementById("btn")
+  function Encode(str) {
+    let morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+    let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let len = str.length
+    let result = []
+    let s = str.toLowerCase()
+      .split(" ")
+      .map((v) => alphabets.indexOf(v) + 1)
+    for (let i = 0; i < len; i++) {
+      if (s[i] > 0) {
+        let val = s[i] - 1
+        result.push(morse[val])
+      }
+    }
+    return result.join("").toUpperCase()
+  }
+  function Decode(str) {
+    let morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." ,"  "]
+    let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ," "]
+    let len = str.length
+    let result = []
+    let s = str.toLowerCase()
+      .split("")
+      .map((v) => alphabets.indexOf(v) + 1)
+    for (let i = 0; i < len; i++) {
+      if (s[i] > 0) {
+        let val = s[i] - 1
+        result.push(morse[val])
+      }
+    }
+    return result.join(" ").toUpperCase()
+}
+
+txt.addEventListener('input', function textareaValue(event) {
+  p.textContent = Decode(event.target.value)
+function cpy(){
+navigator.clipboard.writeText(Decode(event.target.value))
+}
+
+btn.addEventListener("click" , () => {cpy()})
+})
